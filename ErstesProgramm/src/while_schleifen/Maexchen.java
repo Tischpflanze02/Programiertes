c8713f6228b994fc1aefcaa6c0553694639d3528
Niklas Hainz <niklashainz@gmail.com>
2025-11-13 14:45:55 +0100

    test

--- ErstesProgramm/src/while_schleifen/Maexchen.java
+++ ErstesProgramm/src/while_schleifen/Maexchen.java
@@ -4,8 +4,8 @@ public class Maexchen {
     public static void main(String[] args) {
         int wuerfel1 = 0,  wuerfel2 = 0, maexchen = 0, anzahlwürfe = 0; 
         double punkte = 0; 
-        boolean d = false;
-        while (punkte != 100000) {
+        boolean nichtkombinierbar = false;
+        while (punkte <= 100000) {
             wuerfel1 = (int) (Math.random()* 6 + 1);
             wuerfel2 = (int) (Math.random()* 6 + 1);
             anzahlwürfe = anzahlwürfe +1;
@@ -14,13 +14,13 @@ public class Maexchen {
             if (wuerfel1 == 1  && wuerfel2 == 2 || wuerfel2 == 2 && wuerfel1 == 1){
                 maexchen = maexchen + 1;
                 punkte = punkte + 1000;
-                d = true;
+                nichtkombinierbar = true;
             }
             if (wuerfel1 == wuerfel2) {
                 punkte = punkte + wuerfel1 * 100;
-                d = true;
+                nichtkombinierbar = true;
             }
-            if (d == false) {
+            if (nichtkombinierbar == false) {
                 if (wuerfel1 > wuerfel2) {
                     punkte= punkte + (wuerfel1+wuerfel2);
                 }else{
@@ -28,8 +28,12 @@ public class Maexchen {
                 }
             }
         }
+        double maexchenanzahl =anzahlwürfe/maexchen;
         System.out.println("++++++ Mäxchen +++++++");
         System.out.println("Punkte Endstand: "+ punkte);
+        System.out.println("Anzahl Würfe: "+ anzahlwürfe);
+        System.out.println("Anzahl maexchen: "+ maexchen);
+        System.out.println("Maexchen in Prozent: "+ maexchenanzahl);
         
     }
 }
